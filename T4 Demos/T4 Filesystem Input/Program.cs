3636828405643505f3ad47ed59199809100d9875
Kalle Launiala <kalle.launiala@protonit.net>
2012-03-05 14:09:20 +0200

    Initial demo structure

---
+++ T4 Demos/T4 Filesystem Input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4_Filesystem_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ T4 Demos/T4 SQL Database Schema Input/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4_SQL_Database_Schema_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
