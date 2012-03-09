9fbf1777b4df369e69049a66608842bb88b4aab2
Kalle Launiala <kalle.launiala@protonit.net>
2012-03-09 06:48:48 +0200

    WCF Client and service demo added

---
+++ ADM Demos/WCFClientDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using WCFServiceDemo;
+
+namespace WCFClientDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding basicHttpBinding = new BasicHttpBinding();
+            EndpointAddress endpointAddress = new EndpointAddress("http://localhost:56071/DemoService.svc");
+            IDemoService demoService =
+                new ChannelFactory<IDemoService>(basicHttpBinding, endpointAddress).CreateChannel();
+            string serviceResponse = demoService.GetData(123);
+            Console.WriteLine(serviceResponse);
+            Console.ReadLine();
+        }
+    }
+}
